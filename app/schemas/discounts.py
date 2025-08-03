90607e3c24bf40edcb7d3dc054f6492c37f79ede
Gayratbek <gayratbekakhmedov@gmail.com>
2025-08-03 14:48:48 +0500

    ad

--- app/schemas/discounts.py
+++ app/schemas/discounts.py
@@ -35,7 +35,7 @@ class ProductGet(BaseConfig):
     created_at: Optional[datetime] = None
     category:Optional[GetCategoriesTree]=None
     brand:Optional[BrandGet]=None
-    discounts: Optional[List[DiscountsProducts]] = None  # Assuming products can have multiple discounts
+    # discounts: Optional[List[DiscountsProducts]] = None  # Assuming products can have multiple discounts
     details: Optional[List[ProductDetailsBasicData]] = None  # Assuming products can have details
     materials: Optional[List[materialsGet]] = None  # Assuming products can have multiple materials
 
